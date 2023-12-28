eaf94fbdd5ed4a2f2e449c6d8c6deb1e3a216064
Yuval Niezni <yniezni@fireblocks.com>
2023-12-29 00:39:38 +0200

    np

--- publish.sh
+++ publish.sh
@@ -10,7 +10,7 @@ BOLD='\033[1m'
 NORMAL='\033[00m'
 
 echo -e "🚀 Publishing ${BOLD}$PACKAGE_NAME${NORMAL} to $NPM_REGISTRY_URL\n"
-npm publish --registry $NPM_REGISTRY_URL || exit 1
+# npm publish --registry $NPM_REGISTRY_URL || exit 1
 echo "🎉 Published to $NPM_REGISTRY_URL ..."
 
 echo "✅ DONE"
