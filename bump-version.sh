51797a9219daaa84cb0bd0c33de9394a43a24ab3
Yuval Niezni <yniezni@fireblocks.com>
2024-01-03 16:47:11 +0200

    g

--- bump-version.sh
+++ bump-version.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 if [ ! "$1" ]; then
-  echo "Usage: bump-version.sh <version> [pre-release]       See 'npm version --help' for version conventions"
+  echo "Usage: bump-version.sh <version>       See 'npm version --help' for version conventions"
   exit 1
+fi
+
+if [[ $1 == *"rc"* ]]; then
+  echo "rc, not bumping"
+  exit 0
 fi
 
 if [ -z "$NPM_REGISTRY_URL" ]; then
@@ -14,24 +19,23 @@ PACKAGE_VERSION=$(node -p "require('./package.json').version")
 
 echo "⏫ Bumping to '$1' version ..."
 
-if [ "$2" ]; then
-  NEW_VERSION=$(npm version --no-git-tag-version --preid="$2" "$1") || exit 1
-else
-  NEW_VERSION=$(npm version --no-git-tag-version "$1") || exit 1
-fi
+CURRENT_VERSION=$(npm view --registry $NPM_REGISTRY_URL ${PACKAGE_NAME}@latest version > /dev/null || echo "$PACKAGE_VERSION")
+echo "⌗ Current version: $CURRENT_VERSION"
 
+NEW_VERSION=$(npm version --no-git-tag-version "$1") || exit 1
 echo "⌗ Bumping to: ${NEW_VERSION}"
 
 echo "⌗ Updating CHANGELOG"
 npm i -g auto-changelog
 auto-changelog --hide-credit -l 100
+echo "export const SDK_VERSION = '$(echo "${NEW_VERSION}" | sed 's/^v//')'" > src/version.ts
 
 echo "📝 Committing to GitHub..."
 git fetch
 git checkout --track origin/main
 git config --global user.email "github-actions@github.com"
 git config --global user.name "Github Actions"
-git add package.json CHANGELOG.md
+git add package.json CHANGELOG.md src/version.ts
 git commit --no-verify -m "📦 Release $NEW_VERSION" || exit 1
 
 git push --no-verify || exit 1
--- publish.sh
+++ publish.sh
@@ -3,20 +3,19 @@
 if [ -z "$NPM_REGISTRY_URL" ]; then
   NPM_REGISTRY_URL="https://registry.npmjs.org/"
 fi
-
+if [ -z "$1" ]; then
+  # Default tag if not provided
+  TAG="latest"
+else
+  TAG="$1"
+fi
 PACKAGE_NAME=$(node -p "require('./package.json').name")
 
 BOLD='\033[1m'
 NORMAL='\033[00m'
 
-if [ "$1" == "--tag" ]; then
-  TAG="$2"
-  echo -e "🚀 Publishing ${BOLD}$PACKAGE_NAME${NORMAL} with tag "$TAG" to $NPM_REGISTRY_URL\n"
-  npm publish --tag "$TAG" --registry $NPM_REGISTRY_URL || exit 1
-else
-  echo -e "🚀 Publishing ${BOLD}$PACKAGE_NAME${NORMAL} to $NPM_REGISTRY_URL\n"
-  npm publish --registry $NPM_REGISTRY_URL || exit 1
-fi
+echo -e "🚀 Publishing ${BOLD}$PACKAGE_NAME${NORMAL} to $NPM_REGISTRY_URL\n"
+npm publish --tag "$TAG" --registry $NPM_REGISTRY_URL || exit 1
 
 echo "🎉 Published to $NPM_REGISTRY_URL ..."
 echo "✅ DONE"
